9943151017888e56990fc9d72480c0f69d17b772
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-08-21 21:49:32 +0600

    free credit added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import Banner from './components/Banner/Banner'
+import { useState } from 'react'
 
 function App() {
 
+  const [totalBalance, setTotalBalance] = useState(0);
+
+  const handleTotalBalance = (amount) => {
+    setTotalBalance(totalBalance + amount);
+    console.log(totalBalance);
+  };
+
   return (
     <>
-      <NavBar></NavBar>
-      <Banner></Banner>
+      <NavBar totalBalance = {totalBalance}></NavBar>
+      <Banner handleTotalBalance = {handleTotalBalance}></Banner>
     </>
   )
 }
